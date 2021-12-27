54415cc17ba5894c0b79e003ff2faeeab6b34fec
egeergul <egeergull2001@gmail.com>
2021-12-28 01:49:35 +0300

    add food to db

--- app/src/main/java/com/example/restaurantapp/AddMenuItem.java
+++ app/src/main/java/com/example/restaurantapp/AddMenuItem.java
@@ -2,17 +2,23 @@ package com.example.restaurantapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.restaurantapp.Models.Food;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class AddMenuItem extends AppCompatActivity {
     EditText description;
     EditText price;
     Button add;
     Button cancel;
+    private DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,11 +32,31 @@ public class AddMenuItem extends AppCompatActivity {
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(description.getText().toString().isEmpty() || price.getText().toString().isEmpty()){
+                String descriptionTxt = description.getText().toString();
+                Double priceTxt = new Double(price.getText().toString());
+
+                if(descriptionTxt.isEmpty() || priceTxt == 0.0){
                     Toast.makeText( getApplicationContext(), "No fields can be left empty!", Toast.LENGTH_SHORT);
                     return;
                 }
 
+                mDatabase = FirebaseDatabase.getInstance().getReference();
+                Food user = new Food(descriptionTxt, priceTxt);
+                mDatabase.child("users").setValue(user);
+
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Write a message to the database
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("msgs").child("msg3");
+
+                myRef.setValue("Hello, World!");
+                startActivity(new Intent(getApplicationContext(), EditMenuActivity.class));
+                finish();
             }
         });
 
